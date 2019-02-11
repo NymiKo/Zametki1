83a950ca38eda71e2cdd554b7e3284f454b7d429
Dmitry Kabernik <dimon.kabernik8@gmail.com>
2019-02-11 17:47:08 +0300

    Исправил ошибки, но на выделенная строка не работает

--- app/src/main/java/com/example/zametki1/MainActivity.kt
+++ app/src/main/java/com/example/zametki1/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.zametki1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.IOException
@@ -13,6 +17,14 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
+import android.R.string
+import android.os.AsyncTask.execute
+import android.widget.TextView
+import okhttp3.FormBody
+import okhttp3.RequestBody
+import okhttp3.OkHttpClient
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,33 +39,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendGet() {
-        try{
-            var editLog = findViewById<EditText>(R.id.edit_login)
-            var editPass = findViewById<EditText>(R.id.edit_password)
+        val editLog = findViewById<EditText>(R.id.edit_login)
+        val editPass = findViewById<EditText>(R.id.edit_password)
 
+        if(editLog.length() >= 3) {
+            Log.e("OKHTTP3", "функция POST вызвана")
             val client: OkHttpClient = OkHttpClient()
-            val formBody: RequestBody = FormBody.Builder()
-                .add("Login", editLog.toString())
-                .add("Пароль", editPass.toString())
-                .build()
 
-            val request: Request = Request.Builder()
-                .url("android-test-php.000webhostapp.com/login.php")
+            //параметры запроса
+            val formBody = FormBody.Builder()
+                .add("Login", editLog.text.toString())
+                .add("Password", editPass.text.toString())
+                .build()
+            Log.e("OKHTTP3", "Параметры указаны")
+
+            //создание запроса
+            val request = Request.Builder()
+                .url("http://android-test-php.000webhostapp.com/login.php")
                 .post(formBody)
                 .build()
+            Log.e("OKHTTP3", "Запрос создан")
 
-            try{
-                val response: Response = client.newCall(request).execute()
+            //исполняение запроса
+            try {
+                val response: Response = client.newCall(request).execute()//на этой строке ошибка
 
-                val serverAnswer: String = response.body()!!.string()
-                Toast.makeText(applicationContext, serverAnswer, Toast.LENGTH_SHORT).show()
-            }
-            catch (e: IOException){
+                val serverAnswer: String = response.body().toString()
+                Log.e("OKHTTP3", "Ответ сервера: $serverAnswer")
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
-        }
-        catch (e: IOException){
-            Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_SHORT).show()
+        } else{
+            val wrongLogin = findViewById<TextView>(R.id.wrong_login)
+            wrongLogin.visibility = View.VISIBLE
         }
     }
 }
